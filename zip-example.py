32d7e4814e1d44dde8b598c45a53d10eb1c652ea
JananiVenk <vjananicoder@gmail.com>
2021-07-03 21:31:58 +0530

    Second  commit

--- zip-example.py
+++ zip-example.py
@@ -1,6 +1,7 @@
 list1 = [10, 20, 30, 40]
 list2 = [100, 200, 300, 400]
-list3=zip(list1,list2[::-1])
+list3=zip(list1,list2[::-1]) 
+print("zip example code")
 for i in list3:
   x,y = i
   print(x,y)
